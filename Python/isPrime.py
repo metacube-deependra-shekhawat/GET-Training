b8e7d441f4deab3f661ec41d02a682a213b80aab
Deependra Singh Shekhawat <deependra.shekhawat@metacube.com>
2025-01-07 17:22:31 +0530

    fix

--- Python/isPrime.py
+++ Python/isPrime.py
@@ -1,7 +1,7 @@
 num = int(input("Enter the number: "))
 isPrime = True
 for x in range(1,num-1):
-    if(not num % 2):
+    if(not num % x):
         isPrime = False;
         break;
 print(isPrime)
